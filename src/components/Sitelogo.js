bd02a8fd61e3dfaa76ac7e87a60a40dd78d5f5a1
aniruddha001 <aniruddhamahamulkar@gmail.com>
2025-09-10 11:09:06 +0530

    I have remove a from logo link and added Link

--- src/components/Sitelogo.js
+++ src/components/Sitelogo.js
@@ -1,12 +1,13 @@
 import React from 'react'
 import Image from 'next/image'
+import Link from 'next/link';
 
 const Sitelogo = () => {
     return (
         <div className="flex-shrink-0 text-2xl font-bold">
-            <a href='/'>
-                <img src='Logo.png' alt='Mevadhi' width={150} height={40} />
-            </a>
+            <Link href='/'>
+                <Image src='/Logo.png' alt='Mevadhi' width={150} height={40} />
+            </Link>
         </div>
     )
 }
